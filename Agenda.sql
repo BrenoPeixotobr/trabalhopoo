792753aa13a43b166b957eb39e324a37f72fb11c
BrenoPeixotobr <breno.peixoto.santos@gmail.com>
2016-08-02 11:23:16 -0300

    banco de dados

---
+++ Agenda.sql
@@ -0,0 +1,95 @@
+/*create table cliente(
+    cpf integer not null,
+    nome varchar(100) not null,
+    rg varchar(20) not null,
+    email varchar(100)
+    constraint pk_cli primary key (cpf),
+    constraint pf_dep_proj foreign key (cd_dep)
+        references empresa.departamento (codigo)
+        on delete restrict on update cascade
+);*/
+
+create table pessoa(
+    cpf integer not null,
+    nome varchar(100) not null,
+    rg varchar(20) not null,
+    email varchar(100),
+    constraint pk_cli primary key (cpf)
+);
+
+create table telefone(
+    cpf_pessoa integer not null,
+    zap boolean default false,
+    constraint pf_tel_pe foreign key (cpf_pessoa)
+        references pessoa (cpf)
+        on delete restrict on update cascade
+);
+
+create table endereco(
+    cpf_pessoa integer not null,
+    rua varchar(100) not null,
+    numero int,
+    complemento varchar(19),
+    bairro varchar(100) not null,
+    cidade varchar(100) default "Montes Claros",
+    uf varchar(8) default "MG",
+    cep varchar(15), 
+    constraint pf_cpf foreign key (cpf_pessoa)
+        references pessoa (cpf)
+        on delete restrict on update cascade
+);
+
+create table cliente(
+   cpf_pessoa integer not null,
+    genero varchar(15),
+constraint pf_cpf_cli foreign key (cpf_pessoa)
+        references pessoa (cpf)
+        on delete restrict on update cascade
+);
+
+create table cargo(
+   codigo int auto_increment  not null,
+    nome varchar(15),
+    constraint pk_cargo primary key (codigo)
+);
+
+create table funcinario(
+    matricula int auto_increment  not null,
+    cpf_pessoa integer not null,
+    cargo int not null,
+    nivel int default 1,
+        constraint pk_fun primary key (matricula),
+        
+        constraint pf_cpf_fun foreign key (cpf_pessoa)
+        references pessoa (cpf)
+        on delete restrict on update cascade,
+        
+        constraint pf_cargo foreign key (cargo)
+        references cargo (codigo)
+        on delete restrict on update cascade
+);
+
+create table tiposervico(
+   codigo int auto_increment  not null,
+    nome varchar(15),
+    duracao time,
+    valor float not null,
+    constraint pk_ts primary key (codigo)
+);
+
+create table servico(
+    cpf_pessoa integer not null,
+    codigo_funcinario int not null,
+    desconto float default 0.00,
+    data_aten date not null,
+    hora_aten time not null,
+    pago boolean,
+    
+        constraint pfcpfcli foreign key (cpf_pessoa)
+        references cliente (cpf_pessoa)
+        on delete restrict on update cascade,
+        
+        constraint pf_codigo_fun foreign key (codigo_funcinario)
+        references funcinario (matricula)
+        on delete restrict on update cascade
+);
