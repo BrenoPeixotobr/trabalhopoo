42cbe75b4d7958ecfd592510ae273a81b6dd5654
yoskoslowich <yoskos@outlook.com>
2016-08-16 14:21:29 -0300

    Corrigendo Funcinario para Funcionario

--- src/backend/Agenda.java
+++ src/backend/Agenda.java
@@ -12,97 +12,58 @@ public class Agenda {
 	public void comentario(){
 		System.out.println("Nada!...");
 	}
-	
+
 	public Pessoa getP() {
 		return p;
 	}
-
-
-
 
 	public void setP(Pessoa p) {
 		this.p = p;
 	}
 
-
-
-
 	public Funcionario getF() {
 		return f;
 	}
 
-
-
-
-	public void setF(Funcinario f) {
+	public void setF(Funcionario f) {
 		this.f = f;
 	}
-
-
-
 
 	public Telefone getTele() {
 		return tele;
 	}
 
-
-
-
 	public void setTele(Telefone tele) {
 		this.tele = tele;
 	}
-
-
-
 
 	public Cliente getC() {
 		return c;
 	}
 
-
-
-
 	public void setC(Cliente c) {
 		this.c = c;
 	}
-
-
-
 
 	public Endereco getE() {
 		return e;
 	}
 
-
-
-
 	public void setE(Endereco e) {
 		this.e = e;
 	}
-
-
-
 
 	public Servico getS() {
 		return s;
 	}
 
-
-
-
 	public void setS(Servico s) {
 		this.s = s;
 	}
 
-
-
-
 	public TipoServico getTs() {
 		return ts;
 	}
-
-
-
 
 	public void setTs(TipoServico ts) {
 		this.ts = ts;
